b69d20767f02b63922d155cb7abd540d0ad2648c
Yurina Shin <yurinas@gmail.com>
2020-04-16 01:36:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,10 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (13 <= age && age <= 19){
+    return 'You are a teenager!'
+  }
+    return 'You are not a teenager'
 }
 
 function ageChecker(age) {
